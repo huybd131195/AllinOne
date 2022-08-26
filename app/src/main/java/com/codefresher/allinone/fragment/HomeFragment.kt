0defdcc24f6e31da514a98f62cb671a69f441c09
ACER <aniviakid13@gmail.com>
2022-08-26 10:56:05 +0700

    Initial commit

---
+++ app/src/main/java/com/codefresher/allinone/fragment/HomeFragment.kt
@@ -0,0 +1,41 @@
+package com.codefresher.allinone.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.codefresher.allinone.R
+import com.codefresher.allinone.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
