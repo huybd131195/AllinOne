173ecfabe6f48aed5e927b2af882230b9b5f6af5
ACER <aniviakid13@gmail.com>
2022-08-31 11:39:24 +0700

    selecting image from gallery

--- app/src/main/java/com/codefresher/allinone/fragment/create/AddCreateFragment.kt
+++ app/src/main/java/com/codefresher/allinone/fragment/create/AddCreateFragment.kt
@@ -1,17 +1,30 @@
 package com.codefresher.allinone.fragment.create
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
 import androidx.navigation.fragment.findNavController
 import com.codefresher.allinone.R
 import com.codefresher.allinone.databinding.FragmentAddCreateBinding
 import com.codefresher.allinone.databinding.FragmentCreateBinding
 import com.codefresher.allinone.model.Users
 import com.google.firebase.database.*
+import com.squareup.picasso.Picasso
 
 
 class AddCreateFragment : Fragment() {
@@ -20,6 +33,9 @@ class AddCreateFragment : Fragment() {
     private val binding get() = _binding!!
     private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
     private val myReference: DatabaseReference = database.reference.child("MyUsers")
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    var imageUri: Uri? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,11 +49,57 @@ class AddCreateFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        registerActivityForResult()
+
         binding.btnAddUser.setOnClickListener {
             addUserToDatabase()
         }
 
+        binding.imgChose.setOnClickListener {
+            chooseImage()
+        }
+
     }
+
+    fun chooseImage() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1
+            )
+        } else {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            activityResultLauncher.launch(intent)
+        }
+    }
+
+    fun registerActivityForResult() {
+        activityResultLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.StartActivityForResult(),
+                ActivityResultCallback { result ->
+
+                    val resultCode = result.resultCode
+                    val imageData = result.data
+
+                    if (resultCode == Activity.RESULT_OK && imageData != null) {
+                        imageUri = imageData.data
+
+                        imageUri?.let {
+                            Picasso.get().load(it).into(binding.imgChose)
+                        }
+                    }
+                })
+    }
+
 
     private fun addUserToDatabase() {
         val name: String = binding.edtName.text.toString()
