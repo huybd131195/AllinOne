a0d04d4e5cde07077355ebb87f3f2c70be498472
ACER <aniviakid13@gmail.com>
2022-08-31 13:34:21 +0700

    showing the img saved cloud storage in recycler view

--- app/src/main/java/com/codefresher/allinone/adapter/UsersAdapter.kt
+++ app/src/main/java/com/codefresher/allinone/adapter/UsersAdapter.kt
@@ -5,11 +5,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.codefresher.allinone.R
 import com.codefresher.allinone.databinding.CreateItemBinding
 import com.codefresher.allinone.model.Users
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import java.lang.Exception
 
 class UsersAdapter(
     var context: Context,
@@ -39,6 +43,17 @@ class UsersAdapter(
 
     override fun onBindViewHolder(holder: UsersViewHolder, position: Int) {
         holder.binItem(userList[position])
+        val imageUrl = userList[position].url
+        Picasso.get().load(imageUrl).into(holder.adapterBinding.imgView, object : Callback{
+            override fun onSuccess() {
+                Toast.makeText(context,"success",Toast.LENGTH_LONG).show()
+            }
+
+            override fun onError(e: Exception?) {
+                Toast.makeText(context,e?.localizedMessage,Toast.LENGTH_LONG).show()
+            }
+
+        })
         holder.itemView.setOnClickListener {
             onItemClick?.invoke(userList[position])
             val tvN: TextView? = it.findViewById(R.id.tvName)
